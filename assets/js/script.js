2517530352bf3aa6a32f64dcae7a087dd91ab196
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-01-28 01:09:43 +0000

    js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
-
+$(document).ready(function () {
+    //the function to create the date and time for the header
+    function displayDate() {
+      $("#currentDay").html(moment().format("LLLL"));
+    }
+    //updates the time/date each second.
+    setInterval(displayDate, 1000); 
   
-   
+    //local storage function
+    $(".saveBtn").click(function () {
+      var input = $(this).siblings(".time-block").val(); //variable to store the user data
+      var whichHour = $(this).siblings(".time-block").attr("id"); //variable determining which timeblock
+      localStorage.setItem(whichHour, input); //saves the two together in local storage
+    });
+  
+    //hour by hour color changing function
+    var currentHour = moment().hours(); //variable for the current time
+    $(".time-block").each(function () {
+      var calendarHour = parseInt($(this).attr("id")); //variable to compare to the current time
+  
+      //conditions to determine what color to display
+      if (calendarHour === currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+      } else if (calendarHour > currentHour) {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      } else {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).prop("disabled", true);
+      }
+    });
+  
+    //pulling stored data from Storage and back in the timeblocks
+    $("#8").val(localStorage.getItem("8"));
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#13").val(localStorage.getItem("13"));
+    $("#14").val(localStorage.getItem("14"));
+    $("#15").val(localStorage.getItem("15"));
+    $("#16").val(localStorage.getItem("16"));
+  });
+  
